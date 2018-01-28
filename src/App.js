4bb594f930ee4911f7feae36cad5e3e5fcd61a00
mpro34 <mpro34@gmail.com>
2018-01-28 15:54:34 -0800

    fix: Issue rendering proper title and authors, pass props instead of state

--- src/App.js
+++ src/App.js
@@ -11,13 +11,18 @@ import BookSearch from './components/BookSearch';
 class BooksApp extends React.Component {
   state = {
     books: [
-      <Book />,
-      <Book />
+      // <Book
+      //   title='Test Book 1'
+      //   authors='Test Authors 1' imgUrl={'http://books.google.com/books/content?id=bUybAgAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'}/>,
+      // <Book     imgUrl={'http://books.google.com/books/content?id=bUybAgAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'}
+      // title={'Test Book 2'}
+      // authors={'Test Authors 2'}
+      // />
     ] //Array of Book Objects
   }
 
   render() {
-    console.log(`Start = ${this.state.books}`)
+    console.log(`Start = ${JSON.stringify(this.state.books)}`)
     return (
         <div className="app">
           <Route exact path='/' render={() => (
--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -3,22 +3,12 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 //import BookShelf from './BookShelf';
 
-class BookSearch extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      books: []
-    }
-  }
-
-  render() {
-    return (
-     <div className="search-books">
-       <SearchBar />
-     </div>
-    );
-  };
+function BookSearch () {
+  return (
+    <div className="search-books">
+      <SearchBar />
+    </div>
+  );
 }
 
 export default BookSearch;
