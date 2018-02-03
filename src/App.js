a7c2cc45b74ae77aae543cba8b5dbf3c41574709
mpro34 <mpro34@gmail.com>
2018-02-02 17:07:42 -0800

    feat: Add 404 page to Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom';
+import { Switch } from 'react-router';
 import './App.css'
 
 import BookShelf from './components/BookShelf';
 import BookSearch from './components/BookSearch';
+import NoMatch from './components/NoMatch';
 import * as BooksAPI from './BooksAPI'
 
 
@@ -28,40 +30,42 @@ class BooksApp extends React.Component {
     this.updateBookState();
   }
 
-  componentWillUpdate() {
+  componentWillReceiveProps() {
     this.updateBookState();
   }
 
   render() {
     return (
         <div className="app">
-          <Route exact path='/' render={() => (
-            <div className="list-books">
-              <div className="list-books-title">
-                <h1>MyReads</h1>
+          <Switch>
+            <Route exact path='/' render={() => (
+              <div className="list-books">
+                <div className="list-books-title">
+                  <h1>MyReads</h1>
+                </div>
+                <BookShelf
+                  title="Currently Reading"
+                  shelfId="currentlyReading"
+                  books={this.state.books.filter(book => book.shelf === "currentlyReading")}
+                />
+                <BookShelf
+                  title="Read"
+                  shelfId="read"
+                  books={this.state.books.filter(book => book.shelf === "read")}
+                />
+                <BookShelf
+                  title="Want to Read"
+                  shelfId="wantToRead"
+                  books={this.state.books.filter(book => book.shelf === "wantToRead")}
+                />
+                <div className="open-search">
+                  <Link to="/search">Add a book</Link>
+                </div>
               </div>
-              <BookShelf
-                title="Currently Reading"
-                shelfId="currentlyReading"
-                books={this.state.books.filter(book => book.shelf === "currentlyReading")}
-              />
-              <BookShelf
-                title="Read"
-                shelfId="read"
-                books={this.state.books.filter(book => book.shelf === "read")}
-              />
-              <BookShelf
-                title="Want to Read"
-                shelfId="wantToRead"
-                books={this.state.books.filter(book => book.shelf === "wantToRead")}
-              />
-              <div className="open-search">
-                <Link to="/search">Add a book</Link>
-              </div>
-            </div>
-          )}/>
-
-          <Route path='/search' component={BookSearch} />
+            )}/>
+            <Route path='/search' component={BookSearch} />
+            <Route component={NoMatch} status={404}/>
+          </Switch>
         </div>
     );
   };
---
+++ src/components/NoMatch.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NoMatch = () => (
+  <div>Opps, nothing is here!</div>
+);
+
+export default NoMatch;
